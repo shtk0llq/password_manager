509c1abe01e929d51490936a631cb0d43ef154f1
shtk0119 <shtk0119@icloud.com>
2023-11-22 19:13:39 +0900

    ステップ2 修正

--- password_manager.sh
+++ password_manager.sh
@@ -16,17 +16,17 @@ do
       echo "パスワードを入力してください："
       read password
       
-      echo "$service_name:$user_name:$password" >> password_manager.sh
+      echo "$service_name:$user_name:$password" >> passwords.txt
 
       echo "パスワードの追加は成功しました。"
       ;;
     "Get Password")
       echo "サービス名を入力してください："
       read service_name
-      grep -q "^$service_name:" password_manager.sh
+      grep -q "^$service_name:" passwords.txt
 
       if [ $? -eq 0 ]; then
-        service=$(grep "^$service_name:" password_manager.sh)
+        service=$(grep "^$service_name:" passwords.txt)
         IFS=":"
         read service_name user_name password <<< "$service"
 
