c638984db020b72b6672086804f4e613363a54e7
shtk0119 <shtk0119@icloud.com>
2023-11-24 17:02:48 +0900

    ステップ3

--- password_manager.sh
+++ password_manager.sh
@@ -1,5 +1,9 @@
 echo "パスワードマネージャーへようこそ！"
 
+if [ ! -e secret.key ]; then
+  openssl rand -out secret.key 32
+fi
+
 while [ "$input" != "Exit" ]
 do
   echo "次の選択肢から入力してください(Add Password/Get Password/Exit)："
@@ -16,13 +20,23 @@ do
       echo "パスワードを入力してください："
       read password
       
-      echo "$service_name:$user_name:$password" >> passwords.txt
+      if [ -e encrypted.txt ]; then
+        openssl enc -d -aes-256-cbc -pbkdf2 -in encrypted.txt -out passwords.txt -pass file:./secret.key
+        echo "$service_name:$user_name:$password" >> passwords.txt
+        openssl enc -e -aes-256-cbc -pbkdf2 -in passwords.txt -out encrypted.txt -pass file:./secret.key
+      else
+        echo "$service_name:$user_name:$password" >> passwords.txt
+        openssl enc -e -aes-256-cbc -pbkdf2 -in passwords.txt -out encrypted.txt -pass file:./secret.key
+      fi
 
+      rm passwords.txt
       echo "パスワードの追加は成功しました。"
       ;;
     "Get Password")
       echo "サービス名を入力してください："
       read service_name
+      openssl enc -d -aes-256-cbc -pbkdf2 -in encrypted.txt -out passwords.txt -pass file:./secret.key
+
       grep -q "^$service_name:" passwords.txt
 
       if [ $? -eq 0 ]; then
@@ -36,6 +50,8 @@ do
       else
         echo "そのサービスは登録されていません"
       fi
+
+      rm passwords.txt
       ;;
     "Exit")
       echo "Thank you!"
